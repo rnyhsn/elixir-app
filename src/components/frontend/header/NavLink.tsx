753679b0e4c53cbcb9c21eb2e7183e8085e9e3f1
Rony Hossain <mdrony.ain@gmail.com>
2024-09-19 17:34:11 +0600

    second commit

--- src/components/frontend/header/NavLink.tsx
+++ src/components/frontend/header/NavLink.tsx
@@ -1,12 +1,18 @@
+"use client";
 import Link from 'next/link'
 import { ILink } from './Navbar'
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 
 const NavLink = ({item}: {item: ILink }) => {
+  const pathname = usePathname();
+  const [selected, setSelected] = useState<any>(pathname);
+  
   return (
     <div className={`relative ${item.sublink && "group"}`}>
-        <Link href={item.path ? item.path : ""} className={`text-primary tracking-wide font-semibold flex items-center gap-1`}>
+        <Link onClick={()=> setSelected(item.path)} href={item.path ? item.path : ""} className={`text-primary tracking-wide font-semibold flex items-center gap-1 hover:text-secondary ${selected === item.path ? "text-secondary" : ""}`}>
             {item.title} { item.sublink && <MdKeyboardArrowDown /> } 
         </Link>
     {
